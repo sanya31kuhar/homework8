7623452a3dd186ce674741ad12c545f4106c3dcd
Alexander <kuhar@mail.ru>
2022-08-19 02:35:45 +0300

    Выполнил ДЗ номер 8 (Строки)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    // task 1
+        String firstName = "Ivan";
+        String middleName ="Ivanovich";
+        String lastName ="Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // task 2
+        fullName = "Ivanov Ivan Ivanovich";
+        String fullNames = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNames);
+
+        // task 3
+        fullName = "Иванов Семён Семёнович";
+        fullName=fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
     }
 }
